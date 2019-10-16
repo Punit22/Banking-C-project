e9fca53a29e2f40ab216ecac2c3afd8088e54809
Punit22 <shelarpuneet@gmail.com>
2019-10-16 23:56:58 +0530

    Add files via upload

---
+++ welcome.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void jio();
+ /*int main()
+ {
+    welcome();
+    getch();
+    return 0;
+  }
+  */
+
+
+
+  void welcome()
+  {
+
+  cout<<"\n\n\n\n\n\n\n ";
+  system("color A");
+    cout<<"\t\t\t\t\t\t    Welcome  "<<endl;
+
+  cout<<endl;
+  sleep(1);
+  system("color E");
+  cout<<endl<<endl<<endl<<endl<<endl;
+    cout<<"\t\t\t\t\t\t    To  "<<endl;
+
+  sleep(1);
+  system("color B");
+  cout<<endl<<endl<<endl<<endl<<endl;
+  jio();
+  sleep(1);
+  system("color BC");
+  sleep(1);
+  system("color 7");
+  }
+
+ void jio()
+ {
+
+  cout<<"\t\t\t\t\t\t   Our Bank  "<<endl;
+
+
+ }
